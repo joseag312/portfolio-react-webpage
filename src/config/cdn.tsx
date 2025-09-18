6a03dea2f232f6b40f43650e5804e9f4bc54861f
Jose Azuara <joseag312@gmail.com>
2025-09-18 07:49:30 -0700

    Mobile Implementation Part4

--- src/config/cdn.tsx
+++ src/config/cdn.tsx
@@ -1,4 +1,4 @@
 export const CDN_BASE = "https://d2c3htaz4f69zk.cloudfront.net";
-export const CDN_VERSION = "v2";
+export const CDN_VERSION = "v3";
 export const CDN = `${CDN_BASE}/projects/${CDN_VERSION}`;
 export const LOGO = `${CDN}/logo.png`;
